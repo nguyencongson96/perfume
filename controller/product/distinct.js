dd28a5722369dfdf5107b396234d5432d6c14d38
chansen2912 <chansen2912@gmail.com>
2023-04-07 23:46:28 +0700

    third commit

--- controller/product/distinct.js
+++ controller/product/distinct.js
@@ -15,12 +15,20 @@ const getDistinctList = async (req, res) => {
     // If the "field" parameter exists but is not a string or contains invalid elements, throw an error with status code 400 and message "Invalid Param"
     typeof field !== "string" && _throw(400, "Invalid Param");
 
-    // Loop through each element of the "fieldArr" array and set a property in the "result" object with the same name as the element and assign to it an array of distinct values of that field obtained from a MongoDB query using the Mongoose model named "Products"
-    const result = {};
-    for (const item of field.split("-")) {
-      !distinctKey.includes(item) && _throw(400, "Invalid Param");
-      result[item] = await Products.distinct(item);
-    }
+    // Send all promise at once to get the distinct item and get return as array of array
+    const fieldArr = field.split("-"),
+      uniqueArr = await Promise.all(
+        fieldArr.map((item) => {
+          !distinctKey.includes(item) && _throw(400, "Invalid Param");
+          return Products.distinct(item);
+        })
+      );
+
+    //Convert array of array to obj of array
+    const result = uniqueArr.reduce((obj, unique, index) => {
+      const newField = { [fieldArr[index]]: unique };
+      return { ...obj, ...newField };
+    }, {});
 
     // Send a JSON response with status code 200 containing the "result" object
     res.status(200).json(result);
