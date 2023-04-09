1d2ee5e6166b8f9dde1a275fb892beefb4869493
chansen2912 <chansen2912@gmail.com>
2023-04-09 19:26:11 +0700

    third commit

--- routes/product/productCRUD.route.js
+++ routes/product/productCRUD.route.js
@@ -18,16 +18,14 @@ const {
 router.route("/:id").get(getAProduct);
 router.route("/").get(getAllProduct);
 
+//Route get an array of unique items of a field
+router.route("/distinct/:field").get(getDistinctList);
+
 //Need accessToken to do the next route
 router.use(verifyJWT, verifyRoles(ROLES_LIST.Admin));
 
 //Route get detail of all products, add a new product, update a product
-router
-  .route("/")
-  // .get(getAllProduct)
-  .post(addProduct)
-  .patch(updateProduct)
-  .put(test);
+router.route("/").post(addProduct).patch(updateProduct).put(test);
 
 //Route delete a product
 router.route("/:id").delete(deleteProduct);
--- routes/product/productFilter.route.js
+++ routes/product/productFilter.route.js
@@ -9,7 +9,4 @@ router.route("/").get(getProductsByFilter.byField);
 //Route search
 router.route("/search").get(getProductsByFilter.bySearchName);
 
-//Route get an array of unique items of a field
-router.route("/distinct/:field").get(getDistinctList);
-
 export default router;
