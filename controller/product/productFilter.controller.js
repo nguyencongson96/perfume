c8791428c8ce74c1a707616c8594168af464529e
chansen2912 <chansen2912@gmail.com>
2023-04-10 22:43:25 +0700

    fourth commit

--- controller/product/productFilter.controller.js
+++ controller/product/productFilter.controller.js
@@ -1,5 +1,4 @@
 import Products from "../../model/products.model.js";
-import mergeSort from "./sort.js";
 import Pagination from "../../config/filter/pagination.config.js";
 import keyQuery from "../../config/filter/filterKey.config.js";
 import sortConfig from "../../config/filter/sortStyle.config.js";
@@ -7,117 +6,125 @@ import _throw from "../throw.js";
 
 const { limit } = Pagination;
 
-// This function takes a string as an argument and returns an object with a comparison operator and a number.
-const compareSymbol = (arr, key) => {
-  if (keyQuery.numberCompare.includes(key))
-    return arr.map((item) => {
-      return {
-        [key]:
-          item.slice(0, 2) === ">="
-            ? { $gte: Number(item.slice(2)) }
-            : item.slice(0, 2) === "<="
-            ? { $lte: Number(item.slice(2)) }
-            : item.slice(0, 1) === ">"
-            ? { $gt: Number(item.slice(1)) }
-            : item.slice(0, 1) === "<"
-            ? { $lt: Number(item.slice(1)) }
-            : { $eq: Number(item) },
-      };
-    });
+/**
+ * Converts an array of values to an array of objects that can be used in a MongoDB query.
+ * @param {Array} arr - The array of values to convert.
+ * @param {String} key - The key to use in the objects.
+ * @returns {Array} An array of objects that can be used in a MongoDB query.
+ */
+const convertMatchCond = (arr, key) => {
+  // Object containing comparison symbols
+  const compareSymbol = {
+    min: "$gte",
+    max: "$lte",
+    greater: "$gt",
+    lower: "$lt",
+  };
+  // If the key contains a number comparison symbol
+  if (keyQuery.numberCompare.some((item) => key.includes(item))) {
+    // Find the comparison symbol in the key
+    const foundCompare = Object.keys(compareSymbol).find((compare) =>
+      key.includes(compare)
+    );
+    return foundCompare
+      ? arr.map((item) => ({
+          [key.replace(foundCompare, "")]: {
+            [compareSymbol[foundCompare]]: Number(item),
+          },
+        })) // Return an array of objects with the comparison symbol and number if a comparison symbol was found
+      : arr.map((item) => ({ [key]: { $eq: Number(item) } })); // Return an array of objects with a number that is equal to the item
+  }
+  // If the key contains an include comparison symbol
   else if (keyQuery.includeCompare.includes(key))
-    return arr.map((item) => {
-      return { [key]: new RegExp(item, "i") };
-    });
-  else
-    return arr.map((item) => {
-      return { [key]: new RegExp(`^${item}$`, "i") };
-    });
+    // Return an array of objects with a regular expression
+    return arr.map((item) => ({ [key]: new RegExp(item, "i") }));
+  // If the key does not contain a comparison symbol
+  // Return an array of objects with a regular expression that matches exactly
+  else return arr.map((item) => ({ [key]: new RegExp(`^${item}$`, "i") }));
 };
 
 const getProductsByFilter = async (req, res) => {
   try {
     const query = req.query,
-      queryArr = Object.entries(query),
       keyArr = Object.keys(query);
 
     //Check whether req.query has any key-value pairs or not
     keyArr.length === 0 && _throw(400, "Query Params is required");
 
     //Check whether query has any key match allowKey array, if not send status 400
-    !keyArr.every((val) => keyQuery.filter.includes(val)) &&
+    !keyArr.every((val) => keyQuery.filter.some((key) => val.includes(key))) &&
       _throw(400, "Invalid key Query");
 
     //Check whether have to filter field or not
-    const fieldArr = !query.field ? [] : query.field.split("-");
-    fieldArr.length > 0 &&
-      !fieldArr.every((field) => keyQuery.all.includes(field)) &&
+    query.field &&
+      query.field.split("-").some((field) => !keyQuery.all.includes(field)) &&
       _throw(400, "Invalid field Query");
 
     //Check sort value
-    const sortVal = query.sort;
-    sortVal &&
-      !Object.values(sortConfig).includes(sortVal) &&
+    query.sort &&
+      !Object.values(sortConfig).includes(query.sort) &&
       _throw(400, "Invalid sort Query");
 
     //Check slice value
-    const page = query.page;
-    page && (!Number(page) || page < 1) && _throw(400, "Invalid page Query");
+    query.page &&
+      (!Number(query.page) || query.page < 1) &&
+      _throw(400, "Invalid page Query");
 
     // Find products that match the query object
     const products = await Products.aggregate(
       [
         //Add filter
         {
-          $match: queryArr.reduce((obj, [key, value]) => {
+          $match: Object.entries(query).reduce((obj, [key, value]) => {
             !keyQuery.uncompare.includes(key) &&
               (obj = {
                 ...obj,
                 ...(value.includes("-")
-                  ? { $and: compareSymbol(value.split("-"), key) }
+                  ? { $and: convertMatchCond(value.split("-"), key) }
                   : value.includes(".")
-                  ? { $or: compareSymbol(value.split("."), key) }
-                  : compareSymbol(value.split(), key)[0]),
+                  ? { $or: convertMatchCond(value.split("."), key) }
+                  : convertMatchCond(value.split(), key)[0]),
               });
             return obj;
           }, {}),
         },
         //Get specific fields want to get
-        fieldArr.length === 0
-          ? false
-          : {
-              $project: fieldArr.reduce((obj, val) => {
+        query.field
+          ? {
+              $project: query.field.split("-").reduce((obj, val) => {
                 return { ...obj, [val]: 1 };
               }, {}),
-            },
+            }
+          : false,
         //Get random product
-        !query.random ? false : { $sample: { size: limit } },
+        query.random ? { $sample: { size: limit } } : false,
         //Sort
-        !sortVal
-          ? false
-          : {
+        query.sort
+          ? {
               $sort: {
-                [sortVal.slice(0, 1) === "p" ? "price" : "name"]:
-                  sortVal.slice(1, 3) === "ac" ? 1 : -1,
+                [query.sort.slice(0, 1) === "p" ? "price" : "name"]:
+                  query.sort.slice(1, 3) === "ac" ? 1 : -1,
               },
-            },
-      ].filter((pipe) => pipe !== false)
+            }
+          : false,
+        //Slice
+        query.page > 1 ? { $skip: limit * (query.page - 1) } : false,
+        query.page ? { $limit: limit } : false,
+        //Remove pipeline does not appear in client request
+      ].filter(Boolean)
     );
 
-    const sliceList = !page
-      ? products
-      : products.slice((page - 1) * limit, page * limit);
-
     res.status(200).json({
-      total: sliceList.length,
-      page: !page ? "all" : Number(page),
-      limit: !page ? false : Math.min(limit, sliceList.length),
-      list: sliceList,
+      total: products.length,
+      ...(query.page && { page: Number(query.page) }),
+      ...(query.page && { limit: Math.min(limit, products.length) }),
+      list: products,
     });
   } catch (err) {
     console.log(err);
-    res
-      .status(err.status || 500)
-      .json({ msg: err.msg || "Error occurred while filtering" });
+    err.status
+      ? res.status(err.status).json(err.msg)
+      : res.status(500).json("Error occurred while filtering");
   }
 };
 
