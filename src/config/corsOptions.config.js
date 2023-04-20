d27970bafa20267f0168b7a8b61cb34dd8d51175
chansen2912 <chansen2912@gmail.com>
2023-04-20 16:21:27 +0700

    sixth commit

--- src/config/corsOptions.config.js
+++ src/config/corsOptions.config.js
@@ -1,9 +1,8 @@
-const whiteList = ["http://localhost:3000", "http://localhost:4000"];
+const whiteList = ["http://localhost:3000", "http://localhost:4000", "https://perfume-frontend.onrender.com/"];
 
 const corsOptions = {
   origin: function (origin, callback) {
-    if (!origin || origin === "null" || whiteList.indexOf(origin) !== -1)
-      return callback(null, true);
+    if (!origin || origin === "null" || whiteList.indexOf(origin) !== -1) return callback(null, true);
     else return callback(new Error("Not allowed by CORS"), false);
   },
   methods: ["GET", "PUT", "POST", "DELETE"],
