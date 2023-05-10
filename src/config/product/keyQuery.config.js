e8287c6997bafc3d45604368d3cff2d5e5e49e5a
chansen2912 <chansen2912@gmail.com>
2023-05-10 20:40:54 +0700

    eight commit

--- src/config/product/keyQuery.config.js
+++ src/config/product/keyQuery.config.js
@@ -2,7 +2,7 @@ const keyQuery = {
   all: ["name", "brand", "image", "aroma", "type", "capacity", "price", "stock", "description"],
   uncompare: ["sort", "page", "field", "random"],
   numberCompare: ["stock", "price"],
-  includeCompare: ["name"],
+  includeCompare: ["name", "phone"],
   filter: ["name", "sort", "page", "brand", "aroma", "type", "capacity", "price", "stock", "field", "random"],
   distinct: ["brand", "type", "aroma", "capacity"],
   getList: ["_id", "image", "name", "price", "stock"],
