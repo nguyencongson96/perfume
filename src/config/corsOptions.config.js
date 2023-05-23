c3a061a5e6607f2a8829388495ea09b3143d3d8f
nguyencongson96 <chansen2912@gmail.com>
2023-05-23 09:57:56 +0700

    Second commit

--- src/config/corsOptions.config.js
+++ src/config/corsOptions.config.js
@@ -2,11 +2,10 @@ const whiteList = [];
 
 const corsOptions = {
   origin: function (origin, callback) {
-    if (!origin || origin === "null" || whiteList.indexOf(origin) !== -1)
-      return callback(null, true);
+    if (!origin || origin === "null" || whiteList.indexOf(origin) !== -1) return callback(null, true);
     else return callback(new Error("Not allowed by CORS"), false);
   },
-  methods: ["GET", "PUT", "PATCH", "POST", "DELETE", "OPTION"],
+  methods: ["GET", "PUT", "PATCH", "POST", "DELETE"],
   credentials: true,
 };
 
